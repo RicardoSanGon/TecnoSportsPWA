939452bf8d5373f363e30ce6bc5d85317d949766
Ricardo Ramón Sánchez González <22170038@uttcampus.edu.mx>
2025-12-04 22:02:45 -0600

    Escuchar notificaciones

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  // Asegúrate de que la ruta aquí ('/service-worker.js')
+  // apunte a la ubicación final del archivo después de la compilación.
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado con éxito:", registration);
+      })
+      .catch((error) => {
+        console.error("Fallo en el registro del Service Worker:", error);
+      });
+  });
+}
