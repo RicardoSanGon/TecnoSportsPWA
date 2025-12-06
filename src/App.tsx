f88ed2528874777ca8857d8aeab9280881372d60
Ricardo Ramón Sánchez González <22170038@uttcampus.edu.mx>
2025-12-05 22:38:22 -0600

    Desactive OneSignal

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ import './theme/variables.css';
 
 import React, { useEffect } from 'react';
 import { initializeSupabaseNotifications } from './lib/supabaseNotifications';
-import { initializeOneSignal } from './lib/oneSignal';
+// import { initializeOneSignal } from './lib/oneSignal';
 
 setupIonicReact();
 
@@ -50,7 +50,7 @@ const App: React.FC = () => {
         if (userProfile && userProfile.id) {
           initializeSupabaseNotifications(userProfile.id);
           // Initialize OneSignal for Push Notifications
-          initializeOneSignal(String(userProfile.id));
+          // initializeOneSignal(String(userProfile.id));
         }
       } catch (e) {
         console.error('Error parsing user profile for notifications', e);
